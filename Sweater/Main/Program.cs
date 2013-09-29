6594866d5316370ddc2d55ac0874da609659f591
andysctu <andysctu@gmail.com>
2013-09-28 23:40:30 -0400

    Add Class Sweater

---
+++ Sweater/Main/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call constructor to create new Sweater s1, s2, with appropriate fields
+            Sweater s1 = new Sweater("Turtleneck", "Neon-Brown", 22, 99.99, true);
+            Sweater s2 = new Sweater("V-neck", "Red", 8, 9.93, false);
+
+
+            //Call method to display all fields of s1, then s2
+            Console.WriteLine("Sweater s1");
+            s1.DisplayToConsole();
+
+            Console.WriteLine("Sweater s2");
+            s2.DisplayToConsole();
+        }
+    }
+}
---
+++ Sweater/Sweater/Sweater.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweater
+{
+    public class Sweater
+    {
+        //Declare 5 private fields for object Sweater
+        private string _style, _colours;
+        private int _size;
+        private double _price;
+        private bool _isInStock;
+                
+        //Constructor that takes 5 arguments of types declared above                   
+        public Sweater(string style, string colours, int size, double price, bool isInStock)
+        {
+            _style = style;
+            _colours = colours;
+            _size = size;
+            _price = price;
+            _isInStock = isInStock;
+        }
+
+        //Method that displays all 5 fields of a sweater object, right aligned at 12 characters
+        public void DisplayToConsole()
+        {
+            Console.WriteLine(String.Format("{0,12}: {1}", "Style", _style));
+            Console.WriteLine(String.Format("{0,12}: {1}", "Colours", _colours));
+            Console.WriteLine(String.Format("{0,12}: {1}", "Size", _size));
+            Console.WriteLine(String.Format("{0,12}: {1:F2}", "Price", _price));
+            Console.WriteLine(String.Format("{0,12}: {1}", "In Stock", _isInStock));
+            Console.WriteLine();
+        }
+    }
+}
